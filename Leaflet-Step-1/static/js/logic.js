9a36990a672706dc182be958aaa2f737b56c3b7e
Howard <g8r1fl@gmail.com>
2021-05-21 01:10:16 -0400

    tried to code in the circle code within the createFeatures function but got error

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,33 +2,12 @@
 
 // Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-// "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-//   "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 
 // Perform a GET request to the query URL
 d3.json(queryUrl).then(function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
-  // create circle markers based on magnitudes
-  // define array to hold morkers
-  var quakeMarkers = []
-  // loop through features and create circle markers
-  for (var i=0; i < data.features.length; i++) {
-    quakeMarkers.push(
-      L.circle(data.features[i].geometry.coordinates, {
-        stroke: false,
-        fillOpacity: 0.75,
-        color: "yellow",
-        fillColor: "white",
-        radius: data.features[i].properties.mag
-      })
-    );
-  };
-  createMap(quakeMarkers);
-  console.log("these are the circles: ", quakeMarkers);
-
   console.log(data.features);
-  console.log(data.features.length);
 });
 
 function createFeatures(earthquakeData) {
@@ -36,10 +15,17 @@ function createFeatures(earthquakeData) {
   // Define a function we want to run once for each feature in the features array
   // Give each feature a popup describing the place and time of the earthquake
   function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p><p> Depth  " + feature.geometry.coordinates[2] + "</p>");
-      
-  }
+    // create circle markers based on magnitudes
+    // loop through features and create circle markers
+    layer.circle(feature.geometry.coordinates, {
+      stroke: false,
+      fillOpacity: 0.75,
+      color: "yellow",
+      fillColor: "white",
+      radius: feature.properties.mag
+    }).bindPopup("<h3>" + feature.properties.place +
+    "</h3><hr><p>" + new Date(feature.properties.time) + "</p><p> Depth  " + feature.geometry.coordinates[2] + "</p>");
+  };
 
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
@@ -55,7 +41,7 @@ function createFeatures(earthquakeData) {
 
 
 
-function createMap(earthquakes, quakeMarkers) {
+function createMap(earthquakes) {
 
   // Define streetmap and darkmap layers
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -82,8 +68,7 @@ function createMap(earthquakes, quakeMarkers) {
 
   // Create overlay object to hold our overlay layer
   var overlayMaps = {
-    Earthquakes: earthquakes,
-    QuakeMarkers: quakeMarkers
+    Earthquakes: earthquakes
   };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load
